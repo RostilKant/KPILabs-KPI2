9d6c5e8253a61b83800b92d75d3b7fbc74aa1b42
Rostyslav Ilnytskyi <rostyslav.ilnytskyi@gmail.com>
2019-11-20 18:23:52 +0200

    Db Init

---
+++ AirplaneTicketService/Data/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using AirplaneTicketService.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirplaneTicketService.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder modelBuilder)
+        {
+            modelBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=PlaneTickets;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ AirplaneTicketService/Models/Plane.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirplaneTicketService.Models
+{
+    public class Plane
+    {
+        public int PlaneId { get; set; }
+        public string Country { get; set; }
+        public string Company { get; set; }
+        public uint NumOfSeats { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DayOfLastRepair { get; set; }
+    }
+
+    public class Employee 
+    {
+        public Plane Plane { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+    }
+
+}
